a6328f6f5c08fb612c3ac28eae85bcb7c7954b5c
atakhodjaev.b@gmail.com <atakhodjaev.b@gmail.com>
2023-12-14 23:17:00 +0600

    nav-to-catalog

---
+++ src/app/components/NavToCatalog/NavToCatalog.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './NavToCatalog.module.scss';
+import Link from 'next/link';
+
+const NavToCatalog = () => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.creation}>
+        <span>Производство ветеринарных препаратов</span>
+      </div>
+      <div className={styles.inner}>
+        <Link className={styles.inner_right} href="/">
+          <span>Ветеринарные препараты</span>
+          <img src="/imgs/arrow.svg" alt="arrow" />
+        </Link>
+        <Link className={styles.inner_left} href="/">
+          <span>Корма и кормовые добавки</span>
+          <img src="/imgs/arrow.svg" alt="arrow" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NavToCatalog;
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
-
+import NavToCatalog from './components/NavToCatalog/NavToCatalog';
 export default function Home() {
   return (
-    <></>
-  )
+    <>
+      <NavToCatalog />
+    </>
+  );
 }
