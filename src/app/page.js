a8ab60ab116cf847dad23e6c8a8a4c34859c12e6
Bekzat247 <toktobekzat@gmail.com>
2023-12-16 22:20:44 +0600

    feat: fix conflicts

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 import NavToCatalog from './components/NavToCatalog/NavToCatalog';
 export default function Home() {
   return (
-    <>
+    <div>
       <NavToCatalog />
-    </>
+    </div>
   );
 }
