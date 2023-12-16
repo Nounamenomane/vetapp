fcae56390dc6a2e0b13b2bf5210585bc5b839ee4
nounamenomane <nounamenomane@gmail.com>
2023-12-16 22:22:41 +0600

    fix conflict

--- src/app/redux/index.js
+++ src/app/redux/index.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
+import mainSlice from "./mainSlice";
 // connect redux toolkit
 const reducers = combineReducers({
-
+    main: mainSlice
 })
 
 export const store = configureStore({
