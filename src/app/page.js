d883a0234ebfd998ddf1325bcc45e9799b6e935d
Bekzat247 <toktobekzat@gmail.com>
2023-12-16 21:37:26 +0600

    feat: fix conflicts

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
+import NavToCatalog from "./Components/NavToCatalog/NavToCatalog";
 import PromoBlock from "./Components/PromoBlock/PromoBlock";
 
 export default function Home() {
   return (
-    <PromoBlock/>
+    <div>
+      <NavToCatalog/>
+      <PromoBlock/>
+    </div>
   )
 }
