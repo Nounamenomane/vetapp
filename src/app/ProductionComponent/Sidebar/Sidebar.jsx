237bdd6e4f60fbb0ec87f5488459156abdde2409
nounamenomane <nounamenomane@gmail.com>
2024-01-16 23:42:24 +0600

    fix bag

--- src/app/ProductionComponent/Sidebar/Sidebar.jsx
+++ src/app/ProductionComponent/Sidebar/Sidebar.jsx
@@ -1,46 +1,62 @@
-import React from 'react'
-import styles from './Sidebar.module.scss'
-import Link from 'next/link'
+import React from 'react';
+import styles from './Sidebar.module.scss';
 
 function Sidebar({ onSidebarItemClick }) {
+    const sidebarItems = [
+        {
+            label: 'Ветеренарные препараты',
+            subItems: [
+                'Акушерско-гинекологические препараты',
+                'Антибактериальные препараты',
+                'Нестероидные противовоспалительные средства',
+                'Препараты для профилактики и лечения заболеваний вымени',
+                'Препараты для профилактики и лечения нарушений обмена веществ',
+                'Противопаразитарные противопротозойные препараты',
+                'Прочие препараты',
+                'Средства для наружного применения'
+            ]
+        },
+        {
+            label: 'Кормовые добавки и комбикорма',
+            subItems: [
+                'Белково-витаминно-минеральные добавки',
+                'Брикет-лизунец',
+                'Витаминно-минеральные добавки',
+                'Гепатопротектор',
+                'Заменители цельного молока',
+                'Комбикорма',
+                'Лизунцы мелассированные',
+                'Подкислитель/дезинфектант',
+                'Премиксы',
+                'Прикормки рыболовные',
+                'Раскислитель кормов (буфер)',
+                'Энергетические добавки'
+            ]
+        }
+    ];
+
     return (
         <div className={styles.sidebar}>
-            <div className={styles.item1}>
-                <Link onClick={() => onSidebarItemClick('Ветеренарные препараты')} href='/Production'>
-                    Ветеринарные препараты
-                </Link>
-                <div className={styles.menu}>
-                    <Link onClick={() => onSidebarItemClick('Акушерско-гинекологические препараты')} href='/Production'>Акушерско-гинекологические препараты</Link>
-                    <Link onClick={() => onSidebarItemClick('Антибактериальные препараты')} href='/Production'>Антибактериальные препараты</Link>
-                    <Link onClick={() => onSidebarItemClick('Нестероидные противовоспалительные средства')} href='/Production'>Нестероидные противовоспалительные средства</Link>
-                    <Link onClick={() => onSidebarItemClick('Препараты для профилактики и лечения заболеваний вымени')} href='/Production'>Препараты для профилактики и лечения заболеваний вымени</Link>
-                    <Link onClick={() => onSidebarItemClick('Препараты для профилактики и лечения нарушений обмена веществ')} href='/Production'>Препараты для профилактики и лечения нарушений обмена веществ</Link>
-                    <Link onClick={() => onSidebarItemClick('Противопаразитарные противопротозойные препараты')} href='/Production'>Противопаразитарные противопротозойные препараты</Link>
-                    <Link onClick={() => onSidebarItemClick('Прочие препараты')} href='/Production'>Прочие препараты</Link>
-                    <Link onClick={() => onSidebarItemClick('Средства для наружного применения')} href='/Production'>Средства для наружного применения</Link>
+            {sidebarItems.map((item, index) => (
+                <div key={index} className={styles.item1}>
+                    <div
+                        role="button"
+                        tabIndex="0"
+                        onClick={() => onSidebarItemClick(item.label)}
+                    >
+                        {item.label}
+                    </div>
+                    <div className={styles.menu}>
+                        {item.subItems.map((subItem, subIndex) => (
+                            <a key={subIndex} href='/Production' onClick={() => onSidebarItemClick(subItem)}>
+                                {subItem}
+                            </a>
+                        ))}
+                    </div>
                 </div>
-            </div>   <div className={styles.item1}>
-                <Link onClick={() => onSidebarItemClick('Кормовые добавки и комбикорма')} href='/Production'>
-                    Кормовые добавки и
-                    комбикорма
-                </Link>
-                <div className={styles.menu}>
-                    <Link onClick={() => onSidebarItemClick('Белково-витаминно-минеральные добавки')} href='/Production'>Белково-витаминно-минеральные добавки</Link>
-                    <Link onClick={() => onSidebarItemClick('Брикет-лизунец')} href='/Production'>Брикет-лизунец</Link>
-                    <Link onClick={() => onSidebarItemClick('Витаминно-минеральные добавки')} href='/Production'>Витаминно-минеральные добавки</Link>
-                    <Link onClick={() => onSidebarItemClick('Гепатопротектор')} href='/Production'>Гепатопротектор</Link>
-                    <Link onClick={() => onSidebarItemClick('Заменители цельного молока')} href='/Production'>Заменители цельного молока</Link>
-                    <Link onClick={() => onSidebarItemClick('Комбикорма')} href='/Production'>Комбикорма</Link>
-                    <Link onClick={() => onSidebarItemClick('Лизунцы мелассированные')} href='/Production'>Лизунцы мелассированные</Link>
-                    <Link onClick={() => onSidebarItemClick('Подкислитель/дезинфектант')} href='/Production'>Подкислитель/дезинфектант</Link>
-                    <Link onClick={() => onSidebarItemClick('Премиксы')} href='/Production'>Премиксы</Link>
-                    <Link onClick={() => onSidebarItemClick('Прикормки рыболовные')} href='/Production'>Прикормки рыболовные</Link>
-                    <Link onClick={() => onSidebarItemClick('Раскислитель кормов (буфер)')} href='/Production'>Раскислитель кормов (буфер)</Link>
-                    <Link onClick={() => onSidebarItemClick('Энергетические добавки')} href='/Production'>Энергетические добавки</Link>
-                </div>
-            </div>
+            ))}
         </div>
-    )
+    );
 }
 
-export default Sidebar
+export default Sidebar;
